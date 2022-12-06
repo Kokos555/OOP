19ac1f43f06d0ff4d10ffdb7ec88c7242d88fe6f
novotnyv.03 <novotnyv.03@spst.eu>
2022-12-06 11:32:12 +0100

    P04 skoro hotovo

---
+++ P04/Clovek.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P04
+{
+    internal class Clovek
+    {
+        private string jmeno;
+        private string narozen;
+
+        public Clovek(string jmeno, string narozen)
+        {
+            this.jmeno = jmeno;
+            this.narozen = narozen;
+        }
+
+        public void NastavJmeno(string Nove_jmeno)
+        {
+            jmeno = Nove_jmeno;
+        }
+
+        public string VypisJmeno()
+        {
+            return jmeno;
+        }
+        public int Vek()
+        {
+            int v = 0;
+            DateTime datum = Convert.ToDateTime(narozen);
+            DateTime now = DateTime.Now;
+            v = now.Year - datum.Year;
+            if (datum.AddYears(v) > now)
+            {
+                v--;
+            }
+            return v;
+        }
+
+        public bool Plnolety()
+        {
+            if (Vek() >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public string Starsi(Clovek clovek2)
+        {
+
+        }
+
+        public override string ToString()
+        {
+            
+        }
+    }
+}
