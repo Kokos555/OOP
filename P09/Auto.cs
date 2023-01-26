16d4c2580455ded25f1834d7c649a4dc88367fc9
novotnyv.03 <novotnyv.03@spst.eu>
2023-01-26 09:13:00 +0100

    P09 hotovo

---
+++ P09/Auto.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P09
+{
+    internal class Auto
+    {
+        private string znacka;
+
+        private double spotreba;
+
+        private double ujetoCelkem;
+
+        private DateTime okamzikRozjezdu;
+
+        private double dobaJizdyCelkem;
+
+        public bool Jede { get; private set; }
+
+        public Auto(string znacka, double spotreba)
+        {
+            this.znacka = znacka;
+            this.spotreba = spotreba;
+            ujetoCelkem = 0;
+            dobaJizdyCelkem = 0;
+            Jede = false;
+        }
+
+        public double VratUjeteKm()
+        {
+            return ujetoCelkem;
+        }
+
+        public void Rozjeď()
+        {
+            okamzikRozjezdu = DateTime.Now;
+            Jede = true;
+        }
+
+        public void Zastav(int km)
+        {
+            if (Jede)
+            {
+                Jede = false;
+                dobaJizdyCelkem += (DateTime.Now - okamzikRozjezdu).TotalMinutes;
+                ujetoCelkem += km;
+            }
+        }
+
+        public double Celkova_Spotřeba()
+        {
+            return ujetoCelkem / 100 * spotreba;
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"Detaily auta:\nznačka: {znacka}\nnajeto: {ujetoCelkem} km\nčas jízdy: {dobaJizdyCelkem:F2} minut\nspotřeba: {spotreba}\n" +
+                $"auto spotřebovalo {Celkova_Spotřeba()} litrů paliva\nauto momentálně {(Jede ? "jede" : "nejede")}");
+        }
+    }
+}
---
+++ P09/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P09
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Auto moje;
+        bool click = false;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string znacka = textBox1.Text;
+            double potreba = Convert.ToDouble(textBox2.Text);
+            moje = new Auto(znacka, potreba);
+            MessageBox.Show(string.Format($"{moje.ToString()}"));
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int km = Convert.ToInt32(textBox3.Text);
+            if (!click) { 
+                moje.Rozjeď();
+                click = true;
+            }
+            else { 
+                moje.Zastav(km);
+                click = false;
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(string.Format($"{moje.ToString()}"));
+        }
+    }
+}
