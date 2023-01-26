8d1289eb802868509ce6ce48d6e92287a89812b9
novotnyv.03 <novotnyv.03@spst.eu>
2023-01-26 09:32:26 +0100

    P10 dodělat doma, už na to nemám nervy

---
+++ P10/Ucet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P10
+{
+    public enum BankEnum
+    {
+        Aktivní,
+        Zablokovaný,
+        Zrušení
+    }
+    internal class Ucet
+    {
+        static int pocitadlo = 0;
+
+        private string jmeno;
+
+        private int cislo_uctu;
+    }
+}
